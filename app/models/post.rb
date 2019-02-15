79ba206ae10c6d5d760aeaef0dee92a84612314e
Grzegorz Stachowiak <gregista88@gmail.com>
2019-02-15 12:43:51 +0100

    ...

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   mount_uploaders :photos, PhotosUploader
   validates :title, :content, presence: true
-  mount_uploader :image, ImageUploader
+
 end
--- db/migrate/20190214130959_add_photos_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotosToPost < ActiveRecord::Migration[5.2]
-  def change
-    add_column :posts, :photos, :string
-  end
-end
